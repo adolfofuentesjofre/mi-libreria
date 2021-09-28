df3d9a97f03dc62008d1df3c91e4955a0f51c3df
Adolfo Fuentes <adolfo.fuentes.j@gmail.com>
2021-09-28 16:41:48 -0300

    Se agrega funciones de conexión a SQL server

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mypythonlib',
-    packages=find_packages(include=['mypythonlib']),
+    name='mypythonlib', # nombre de la libreria
+    packages=find_packages(include=['mypythonlib']), # carpeta donde buscar la libreria
     version='0.1.0',
-    description='My first Python library',
+    description='Mi libreria',
     author='Adolfo',
-    license='MIT',
-    install_requires=[],
+    install_requires=[
+        'pandas>=1.3.2',
+        'numpy>=1.21',
+        'holidays>=0.10.1',
+        'scipy>=1.4.1',
+        'pyodbc>=4.0.32',
+        ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==6.2.4'],
     test_suite='tests',
+    classifiers=[
+        "Intended Audience :: Data Scientists and Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"]
 )
--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -1,8 +1,19 @@
 import pytest
 from mypythonlib import lib_clase
+import os
 
 def test_multiplicador_entero_positivo():
     assert lib_clase.multiplicador(2,2) == 4
     
 def test_multiplicador_entero_negativo():
     assert lib_clase.multiplicador(-2,-2) == 4
+    
+def test_feriado():
+    #Test de probar una funcion
+    feri = lib_clase().feriado('2020-01-01')
+    assert feri
+    
+def test_sqlquery():
+    #Test de hacer un query
+    df = lib_clase().sql_imercado("select top 10 * from [imercado]..[dm_bse_clientes]")
+    assert len(df)>0
